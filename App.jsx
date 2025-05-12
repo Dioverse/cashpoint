3aa520cd59ed0ad80d458c40d507e1f93185219f
JohnPaulAchur <idahjohnpaul@gmail.com>
2025-05-12 10:56:34 +0100

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "./global.css"
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-red-300">App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
