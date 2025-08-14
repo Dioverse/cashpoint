554fa143a7b546c942cb168418fe9f41240702f4
JohnPaulAchur <idahjohnpaul@gmail.com>
2025-08-14 12:59:24 +0100

    auth and giftcard api int.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Purchase\CryptoController;
 use App\Http\Controllers\Purchase\GiftcardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+ 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
